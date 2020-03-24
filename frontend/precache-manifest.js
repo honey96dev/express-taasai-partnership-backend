bdeb153346e25b8887d8d6bc5aa86555ec6c5eda
honey96dev <honey96dev@gmail.com>
2020-03-25 06:36:55 +0900

    frontend is added

---
+++ frontend/precache-manifest.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5a65234e7e0f34b060157af48a0a98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e632281fb6657def92",
+    "url": "/static/css/2.chunk.css"
+  },
+  {
+    "revision": "51b93b7e1db8a053baa4",
+    "url": "/static/css/main.chunk.css"
+  },
+  {
+    "revision": "b1e632281fb6657def92",
+    "url": "/static/js/2.chunk.js"
+  },
+  {
+    "revision": "51b93b7e1db8a053baa4",
+    "url": "/static/js/main.chunk.js"
+  },
+  {
+    "revision": "51c6542b176764fcbb70",
+    "url": "/static/js/runtime~main.js"
+  }
+]);
