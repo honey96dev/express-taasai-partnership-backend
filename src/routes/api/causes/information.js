b98c7d94451c412a2777c58b3af75a4e9ba98a22
honey96dev <honey96dev@gmail.com>
2020-03-26 05:43:40 -0700

    cause information is updated wit bankinfo

--- src/routes/api/causes/information.js
+++ src/routes/api/causes/information.js
@@ -15,12 +15,12 @@ import {sprintf} from "sprintf-js";
 const saveProc = async (req, res, nect) => {
   const lang = req.get(consts.lang) || consts.defaultLanguage;
   const langs = strings[lang];
-  let {id, name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event} = req.body;
+  let {id, name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event, bank_name, bank_accountname, bank_shortcode, bank_accountnumber} = req.body;
 
   try {
-    let sql = sprintf("UPDATE %s SET name = $1, address = $2, email = $3, dial_code = $4, phone = $5, manager_title = $6, manager_name1 = $7, manager_name2 = $8, about = $9, join_reason = $10, social_media = $11, social_frequency = $12, major_event = $13 WHERE id = $14", dbTblName.causes);
+    let sql = sprintf("UPDATE %s SET name = $1, address = $2, email = $3, dial_code = $4, phone = $5, manager_title = $6, manager_name1 = $7, manager_name2 = $8, about = $9, join_reason = $10, social_media = $11, social_frequency = $12, major_event = $13, bank_name = $14, bank_accountname = $15, bank_shortcode = $16, bank_accountnumber = $17 WHERE id = $18", dbTblName.causes);
 
-    await db.query(sql, [name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event, id]);
+    await db.query(sql, [name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event, bank_name, bank_accountname, bank_shortcode, bank_accountnumber, id]);
 
     const token = jwt.sign(
       {
@@ -35,7 +35,7 @@ const saveProc = async (req, res, nect) => {
       result: langs.success,
       message: langs.successfullySaved,
       data: {
-        user: {id, name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event, user_type: consts.auth.USER_TYPE.CAUSE},
+        user: {id, name, address, email, dial_code, phone, manager_title, manager_name1, manager_name2, about, join_reason, social_media, social_frequency, major_event, bank_name, bank_accountname, bank_shortcode, bank_accountnumber, user_type: consts.auth.USER_TYPE.CAUSE},
         token,
       },
     });
